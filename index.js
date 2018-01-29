6414ee54f9bb3de2c00995c598e1c337ebff80a9
Yaryna Sorochuk <yarkasorochuk@gmail.com>
2018-01-29 12:41:29 +0000

    Initial

--- index.js
+++ index.js
@@ -12,25 +12,26 @@ let first = parseFloat(prompt('write the first number'));
 let second = parseFloat(prompt('write the second number'));
 let third = parseFloat(prompt('write the third number'));
 
-document.writeln(`rivniannia: a * x * x + b * x + c = 0 `);
+document.writeln(`Quadratic equation: a * x * x + b * x + c = 0 `);
+document.write("<br>");
 
 
-function kvadratic(a,b,c) {
+function quadratic(a,b,c) {
     let D = b * b - 4 * a * c ; 
         if (D < 0) {
-            return 'nemaje rozwiazku';
+            return 'There is no solution';
         } if (D == 0){ 
             let x  = -b / 2 * a;
-            return 'rozwiazok odyn' + x;
+            return 'The only one solution' + x;
         } if (D > 0) {
             let x1 = -b + Math.sqrt(D) / 2 * a;
             let x2 = -b - Math.sqrt(D) / 2 * a;
-            return 'washi rezultaty:x1= '+ x1 + 
-            ' washi rezultaty:x2= ' + x2;
+            return 'Your result: x1= '+ x1 + 
+            'Your result: x2= ' + x2;
         }
 }
 
-document.writeln(kvadratic(first,second,third));
+document.writeln(quadratic(first,second,third));
 
 
 
